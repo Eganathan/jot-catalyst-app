a600f8e32b83ef10b95d36daa426b31008040bd1
Eganathan R <ammasamrith@gmail.com>
2024-08-24 16:56:27 +0530

    chore: jot GET implementation

--- functions/JotServerApp/JotServerApp.java
+++ functions/JotServerApp/JotServerApp.java
@@ -1,6 +1,8 @@
 import java.util.logging.Logger;
 
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Level;
 
 
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.catalyst.advanced.CatalystAdvancedIOHandler;
 import com.zc.component.object.ZCObject;
 import com.zc.component.object.ZCRowObject;
+import com.zc.component.zcql.ZCQL;
 
 public class JotServerApp implements CatalystAdvancedIOHandler {
 	private static final Logger LOGGER = Logger.getLogger(JotServerApp.class.getName());
@@ -46,8 +49,8 @@ public class JotServerApp implements CatalystAdvancedIOHandler {
 					switch (method) {
 						case "GET":{
 							LOGGER.log(Level.INFO,"Jots GET request");
-						
-							break;
+							getBulkKeeps(request, response);
+						break;
 						}
 						case "POST":{
 							LOGGER.log(Level.INFO,"Jots POST request");
@@ -92,40 +95,10 @@ public class JotServerApp implements CatalystAdvancedIOHandler {
 		LOGGER.log(Level.INFO,"Inside Jot Creation Block");
 			try {
 
-				// JSONParser jsonParser = new JSONParser();
-	 			// ServletInputStream requestBody = request.getInputStream();
-	 
-	 			// JSONObject jsonObject = (JSONObject) jsonParser.parse(new InputStreamReader(requestBody, "UTF-8"));
-	 
-	 			// String notes = jsonObject.get("notes").toString();
-	 
-	 			// ZCRowObject row = ZCRowObject.getInstance();
-	 			// row.set("Notes", notes);
-	 
-	 			// ZCRowObject todoItem = ZCObject.getInstance().getTable("TodoItems").insertRow(row);
-	 
-	 			// response.setStatus(200);
-	 			// responseData.put("status", "success");
-	 			// responseData.put("data", new JSONObject() {
-	 			// 	{
-	 			// 		put("todoItem", new JSONObject() {
-	 			// 			{
-	 			// 				put("id", todoItem.get("ROWID").toString());
-	 			// 				put("notes", todoItem.get("Notes").toString());
-	 			// 			}
-	 			// 		});
-	 
-	 			// 	}
-	 			// });
-
-
-				 LOGGER.log(Level.INFO,"Jot creation payload recived 0 "+request.toString());
-
 				JSONParser jsonParser = new JSONParser();
 	 			ServletInputStream requestBody = request.getInputStream();
 	 			JSONObject jsonObject = (JSONObject) jsonParser.parse(new InputStreamReader(requestBody, "UTF-8"));
 				JSONObject jot =  (JSONObject) jsonObject.get("jot");
-				LOGGER.log(Level.INFO,"Jot creation payload recived 2"+jot.toString());
 
 				ZCRowObject row = ZCRowObject.getInstance();
 	 			row.set("title",jot.get("title"));
@@ -158,6 +131,53 @@ public class JotServerApp implements CatalystAdvancedIOHandler {
 			}
 	}
 
+	@SuppressWarnings("unchecked")
+	public void getBulkKeeps(HttpServletRequest request, HttpServletResponse response){
+		LOGGER.log(Level.INFO,"Inside GET BULK JOTS Block");
+			try {
+
+				Integer page = Integer.parseInt(request.getParameter("page"));
+	 			Integer perPage = Integer.parseInt(request.getParameter("per_page"));
+
+				Integer totalEntries = Integer.parseInt(ZCQL.getInstance().executeQuery("SELECT COUNT(ROWID) FROM notes").get(0).get("notes", "ROWID").toString());
+	 			Boolean hasMore = totalEntries > page * perPage;
+
+				LOGGER.log(Level.INFO,"Inside GET BULK: request: page"+page+" per_page="+perPage+" availableEntries="+totalEntries+"hasMore="+hasMore);
+
+				ArrayList<Object> jotItems = new ArrayList<>();
+				
+				//need to add pager but will do it later ;)
+				ZCQL.getInstance().executeQuery(String.format("SELECT ROWID, title, message, CREATEDTIME, MODIFIEDTIME FROM notes")).forEach(
+					row -> {
+							jotItems.add(new HashMap() {
+	 							{
+	 								put("id", row.get("notes", "ROWID").toString());
+									put("created_time", row.get("notes", "CREATEDTIME").toString());
+									put("modified_time", row.get("notes", "MODIFIEDTIME").toString());
+	 								put("title", row.get("notes", "title").toString());
+									put("description", row.get("notes", "message").toString());
+	 							}
+	 						});
+	 					});
+
+						 responseData.put("paging_info", new JSONObject(){
+							{
+								put("page", page);
+								put("has_more", hasMore);
+							}
+						 });
+						 responseData.put("jots",jotItems);
+						 responseData.put("status", "success");
+						 
+						
+						 response.setContentType("application/json");
+						 response.setStatus(200);
+						 response.getWriter().write(responseData.toJSONString());
+			} catch (Exception e) {
+				LOGGER.log(Level.SEVERE,"Exception in JotServerApp",e);
+				errorMessage(response, e,"Unable to Fetch Jots","Fetching Jot Failed (did you provide page and per_page as a query parameter) with an exception :"+e.getLocalizedMessage(), 500);
+			}
+	}
 
 	private void errorMessage(HttpServletResponse response, Exception e, String errorTitle, String errorMessage, int errorCode) {
 		try{
