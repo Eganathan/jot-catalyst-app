1b7df3fabf2e1e690016d3dea7623745a69743f2
Eganathan R <ammasamrith@gmail.com>
2025-07-25 22:12:02 +0530

    chore: added git ignore

--- functions/JotServerApp/.output/JotServerApp.java
+++ functions/JotServerApp/.output/JotServerApp.java
@@ -1,52 +1,23 @@
-import java.util.logging.Logger;
-import java.util.logging.Level;
-import org.json.JSONObject;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.catalyst.advanced.CatalystAdvancedIOHandler;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class JotServerApp implements CatalystAdvancedIOHandler {
-	private static final Logger LOGGER = Logger.getLogger(JotServerApp.class.getName());
-	private JSONObject responseData = new JSONObject();
-	
-	@Override
+    private static final Logger LOGGER = Logger.getLogger(JotServerApp.class.getName());
+
+    @Override
     public void runner(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		try {
-			LOGGER.log(Level.SEVERE,"Jots Request Recived");
-			switch(request.getRequestURI()) {
-				case "/jots/test": {
-					LOGGER.log(Level.SEVERE,"Jots Request routed to /jots");
-					responseData.put("data", new JSONObject() {
-						{
-							put("success","jots/test is working");
-							put("error_message", "Successfully test completed, jots/test is working as expected");
-						}
-					});
-					response.setContentType("application/json");
-					response.setStatus(200);
-					response.getWriter().write(responseData.toString());
-					break;	
-				}
-				default: {
-					LOGGER.log(Level.SEVERE,"Jots Request routed to Default as invalid URL");
-					responseData.put("data", new JSONObject() {
-						{
-						put("error_title","Invalid URL");
-						put("error_message", "You seem to have entered invalid url please try again with valid url");
-					}
-					});
-					response.setContentType("application/json");
-					response.setStatus(404);
-					response.getWriter().write(responseData.toString());
-				}
-			}
-		}
-		catch(Exception e) {
-			LOGGER.log(Level.SEVERE,"Exception in JotServerApp",e);
-			response.setStatus(500);
-			response.getWriter().write("Internal server error");
-		}
-	}
+
+        try {
+            new RESTAppService(request, response);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, e.getLocalizedMessage());
+            ResponseGenerator.initiateErrorMessage(response, 500, "Request failed due to an exception", "Sorry we cannot handle your data now!, we had a exception. E" + e.getLocalizedMessage());
+        }
+    }
 }
--- functions/JotServerApp/JotBulkServices.java
+++ functions/JotServerApp/JotBulkServices.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
